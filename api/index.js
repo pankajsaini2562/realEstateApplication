76785e7b2af69e020cdd7c771a4d0e32f23fd02f
pankajsaini <pankajsaini2762@gmail.com>
2024-03-17 11:55:00 +0530

    create signup

--- api/index.js
+++ api/index.js
@@ -3,16 +3,20 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import User from './models/userModel.js'
 import userRouter from './routes/userRoute.js'
+import authRouter from './routes/authRoute.js'
 dotenv.config()
 const app = express()
+app.use(express.json())
 
 mongoose.connect(process.env.MONGO).then(()=>{
 console.log("connected to database")
-app.listen(3000,()=>{
+app.listen(3008,()=>{
   console.log("server is running succesfully")
 })
 })
 .catch((error)=>{
-console.log(error)
-})
+  console.log(error)
+  })
+
 app.use('/api/user',userRouter)
+app.use('/api/auth',authRouter)
