8eb6b796036db66095a3204cefbc5e33a99dde9a
pankajsaini <pankajsaini2762@gmail.com>
2024-03-17 12:14:13 +0530

    create middleware to handle error and function

--- api/index.js
+++ api/index.js
@@ -20,3 +20,13 @@ app.listen(3008,()=>{
 
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
+app.use((err,req,res,next)=>{
+  const statusCode = err.statusCode || 500
+  const message = err.message || 'Internal server error'
+  return res.status(statusCode).json({
+    success:false,
+    message,
+    statusCode
+  })
+
+})
